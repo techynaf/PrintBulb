b7b6741e646e11c50d52d7521851994e08eec5aa
Sakib Hasan <ajobbalok22@gmail.com>
2016-07-07 00:36:34 +0600

    Added template dialog

--- app/core/designStudio/templateDialog/templateDialogController.js
+++ app/core/designStudio/templateDialog/templateDialogController.js
@@ -1,15 +1,14 @@
 designStudio.controller('templateDialogController', ['$scope', '$mdDialog', 'locals', 'productFromCategoryService', function($scope, $mdDialog, locals, productFromCategoryService){
     $scope.type = locals.type;
-    
     $scope.hide = function() {
         $mdDialog.hide();
     };
     $scope.cancel = function() {
         $mdDialog.cancel();
     };
-    $scope.answer = function(answer) {
-        $mdDialog.hide(answer);
+    $scope.useThisTemplate = function(template){
+        console.log(template);
+        $mdDialog.hide();   
     };
-    
     $scope.products = productFromCategoryService.getProductsFromCategory($scope.type.id);
 }]);
